acde1e21080ad0b7aa0bc52d586e9d38c8938b4e
YasminFel2 <ifelberb@campus.haifa.ac.il>
2018-09-13 15:00:01 -0400

    Initial commit

---
+++ app/src/main/java/com/example/jasmin/mapboxtutorial/MainActivity.java
@@ -0,0 +1,81 @@
+package com.example.jasmin.mapboxtutorial;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import android.support.v7.app.AppCompatActivity;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Mapbox.getInstance(this, "pk.eyJ1IjoiamFzODZmIiwiYSI6ImNqbDE4cjI3aDFkN2szdXBncHllMW9kdzUifQ.XmV0rfaWv6sBGPTUnsnURA");
+        setContentView(R.layout.activity_main);
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                // One way to add a marker view
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(41.885,-87.679))
+                        .title("Chicago")
+                        .snippet("Illinois")
+                );
+            }
+        });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+}
