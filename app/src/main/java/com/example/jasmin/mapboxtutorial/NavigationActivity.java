9ca754d6f7e426e26c1697e2ca140d6773dc931c
YasminFel2 <ifelberb@campus.haifa.ac.il>
2018-09-13 17:31:32 -0400

    navigation- main activity and navigation- not working

---
+++ app/src/main/java/com/example/jasmin/mapboxtutorial/NavigationActivity.java
@@ -0,0 +1,78 @@
+package com.example.jasmin.mapboxtutorial;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mapbox.android.core.location.LocationEngineListener;
+import com.mapbox.android.core.permissions.PermissionsListener;
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+
+// classes needed to initialize map
+// classes needed to add location layer
+// classes needed to add a marker
+// classes to calculate a route
+// classes needed to launch navigation UI
+
+
+public abstract class NavigationActivity extends AppCompatActivity implements LocationEngineListener, PermissionsListener {
+
+
+    private MapView mapView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Mapbox.getInstance(this, getString(R.string.access_token));
+        setContentView(R.layout.activity_main);
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+
+
+    }
+
+
+    // Add the mapView's own lifecycle methods to the activity's lifecycle methods
+    @Override
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+}
